7ef95889c301ea42b77eee95f45d2d4994b0c1c9
Denis Popov <deniolp@gmail.com>
2018-12-12 13:44:28 +0300

    Создание custm element query-element и подключение к нему shadow DOM

--- wc.js
+++ wc.js
@@ -6,6 +6,31 @@
       super();
       
       let shadow = this.attachShadow({mode: 'open'});
+      
+      let wrapper = document.createElement('div');
+      let input = document.createElement('input');
+      let button = document.createElement('button');
+      let script = document.createElement('script');
+      
+      wrapper.setAttribute('class', 'nav-bar__item');
+      input.setAttribute('type', 'text');
+      button.setAttribute('type', 'button');
+      button.textContent = 'Поиск';
+      
+      let placeholder = this.getAttribute('placeholder');
+      let idInput = this.getAttribute('idInput');
+      let idButton = this.getAttribute('idButton');
+      let srcScript = this.getAttribute('srcScript');
+      input.textContent = placeholder;
+      input.id = idInput;
+      button.id = idButton;
+      script.src = srcScript;
+      
+      shadow.appendChild(wrapper);
+      wrapper.appendChild(input);
+      wrapper.appendChild(button);
+      wrapper.appendChild(script);
     }
   }
+  customElements.define('query-element', QueryElement);
 })();
