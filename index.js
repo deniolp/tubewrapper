a13dce0dff7591cb3a9810eb68bce5a5465f65c0
Denis Popov <deniolp@gmail.com>
2018-12-12 11:51:12 +0300

    Реализация подгрузки ютуб контента в iframe только при нажатии на кнопку воспроизведения

--- index.js
+++ index.js
@@ -60,6 +60,58 @@ button.addEventListener('click', function() {
           addVideoToList(video);
         }});
       videoList.appendChild(fragment);
+      findVideos();
     })
     .catch(error => console.log(error));
 });
+
+function findVideos() {
+  let videos = document.querySelectorAll('.video__wrapper');
+  
+  for (var i = 0; i < videos.length; i++) {
+    setupVideo(videos[i]);
+  }
+}
+
+function setupVideo(video) {
+  let link = video.querySelector('.video__link');
+  let image = video.querySelector('.video__image');
+  let button = video.querySelector('.video__button');
+  let id = parseURL(image);
+  
+  video.addEventListener('click', function() {
+    let iframe = createIframe(id);
+    
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+  
+  link.removeAttribute('href');
+  video.classList.add('video-wrapper--enabled');
+}
+
+function parseURL(data) {
+  let regexp = /https:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let url = data.src;
+  let match = url.match(regexp);
+  
+  return match[1];
+}
+
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+  
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__image');
+  
+  return iframe;
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+  
+  return 'https://www.youtube.com/embed/' + id + query;
+}
