c3afd6140410b41c001431ec1e77c4fd408da237
Denis Popov <deniolp@gmail.com>
2018-12-13 15:14:05 +0300

    Большое внедрение кастомных элементов и shadow-dom в проект

--- index.js
+++ index.js
@@ -1,119 +1,152 @@
 'use stricts';
 
 (function () {
-  let videoList = document.querySelector('.videos');
-  let button = document.querySelector('.nav-bar__button');
-  let input = document.querySelector('.nav-bar__input');
-  let fragment = document.createDocumentFragment();
-
-  function addVideoToList(video) {
-    let template = document.querySelector('#template').content.querySelector('.video');
-    
-    fragment.appendChild(createVideoElement(template, video));
-  }
-
-  function createVideoElement(template, object) {
-    let element = template.cloneNode(true);
-    element.querySelector('.video__header').innerText = object.snippet.title;
-    element.querySelector('.video__link').href = 'https://www.youtube.com/watch?v=' + object.id.videoId;
-    element.querySelector('.video__picture').srcset = 'https://img.youtube.com/vi_webp/' + object.id.videoId + '/maxresdefault.webp';
-    element.querySelector('.video__image').src = 'https://img.youtube.com/vi/' + object.id.videoId + '/maxresdefault.jpg';
-    element.querySelector('.video__image').alt = object.snippet.title;
-    
-    return element;
-  }
-
-  function getData(url) {
-    return new Promise(function(resolve, reject) {
-      let xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      
-      xhr.addEventListener('load', function() {
-        if (xhr.status === 200) {
-          let json = xhr.response.items;
-          console.log(json);
-          resolve(json);
-        } else {
-          reject(xhr.statusText);
-        }
-      });
-      
-      xhr.addEventListener('error', function(error) {
-        reject(error);
-      });
-      
-      xhr.open('GET', url);
-      xhr.send();
+  setTimeout (function() {
+    let videoList = document.querySelector('.videos');
+    let shadows = document.querySelectorAll('query-element');
+    let buttons = [];
+    let inputs = [];
+    let shadowRoots = shadows.forEach(function(shadow) {
+      buttons.push(shadow.shadowRoot.querySelector('button'));
+      inputs.push(shadow.shadowRoot.querySelector('input'));
     });
-  }
+    let input = document.querySelector('query-element').shadowRoot.querySelector('input');
+    let fragment = document.createDocumentFragment();
 
-  button.addEventListener('click', function() {
-    let search = input.value;
-    
-    while(videoList.firstChild) {
-      videoList.removeChild(videoList.firstChild);
+    function addVideoToList(video) {
+      let template = document.querySelector('#template').content.querySelector('.video');
+      
+      fragment.appendChild(createVideoElement(template, video));
+    }
+
+    function createVideoElement(template, object) {
+      let element = template.cloneNode(true);
+      element.querySelector('.video__header').innerText = object.snippet.title;
+      element.querySelector('.video__link').href = 'https://www.youtube.com/watch?v=' + object.id.videoId;
+      element.querySelector('.video__picture').srcset = 'https://img.youtube.com/vi_webp/' + object.id.videoId + '/maxresdefault.webp';
+      element.querySelector('.video__image').src = 'https://img.youtube.com/vi/' + object.id.videoId + '/maxresdefault.jpg';
+      element.querySelector('.video__image').alt = object.snippet.title;
+      
+      return element;
+    }
+
+    function getData(url) {
+      return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        
+        xhr.addEventListener('load', function() {
+          if (xhr.status === 200) {
+            let json = xhr.response.items;
+            console.log(json);
+            resolve(json);
+          } else {
+            reject(xhr.statusText);
+          }
+        });
+        
+        xhr.addEventListener('error', function(error) {
+          reject(error);
+        });
+        
+        xhr.open('GET', url);
+        xhr.send();
+      });
     }
     
-    getData('https://www.googleapis.com/youtube/v3/search?part=snippet,id&order=date&channelId=' +  search + '&maxResults=50&key=AIzaSyC9j5myBqjEoydyrootsBO1iqe9-dSpPaA')
-      .then(function (videos) {
-        videos.forEach(function (video) {
-          if (video.id.kind === 'youtube#video') {
-            addVideoToList(video);
-          }});
-        videoList.appendChild(fragment);
-        findVideos();
-      })
-      .catch(error => console.log(error));
-  });
-
-  function findVideos() {
-    let videos = document.querySelectorAll('.video__wrapper');
-    
-    for (var i = 0; i < videos.length; i++) {
-      setupVideo(videos[i]);
-    }
-  }
-
-  function setupVideo(video) {
-    let link = video.querySelector('.video__link');
-    let image = video.querySelector('.video__image');
-    let button = video.querySelector('.video__button');
-    let id = parseURL(image);
-    
-    video.addEventListener('click', function() {
-      let iframe = createIframe(id);
+    buttons.forEach(function(button) {
+      if (button.id === 'channel') {
+        button.addEventListener('click', function() {
+          let search = inputs[0].value;
+          
+          while(videoList.firstChild) {
+            videoList.removeChild(videoList.firstChild);
+          }
+          
+          getData('https://www.googleapis.com/youtube/v3/search?part=snippet,id&order=date&channelId=' +  search + '&maxResults=50&key=AIzaSyC9j5myBqjEoydyrootsBO1iqe9-dSpPaA')
+            .then(function (videos) {
+              videos.forEach(function (video) {
+                if (video.id.kind === 'youtube#video') {
+                  addVideoToList(video);
+                }});
+              videoList.appendChild(fragment);
+              findVideos();
+            })
+            .catch(error => console.log(error));
+        });
+      }
       
-      link.remove();
-      button.remove();
-      video.appendChild(iframe);
+      if (button.id === 'search') {
+        button.addEventListener('click', function() {
+          let search = inputs[1].value;
+          console.log(search);
+          while(videoList.firstChild) {
+            videoList.removeChild(videoList.firstChild);
+          }
+            
+          getData('https://www.googleapis.com/youtube/v3/search?part=snippet,id&q=' +  search + '&maxResults=50&key=AIzaSyC9j5myBqjEoydyrootsBO1iqe9-dSpPaA')
+            .then(function (videos) {
+              videos.forEach(function (video) {
+                if (video.id.kind === 'youtube#video') {
+                  addVideoToList(video);
+                }});
+              videoList.appendChild(fragment);
+              findVideos();
+            })
+            .catch(error => console.log(error));
+          });
+      }
     });
     
-    link.removeAttribute('href');
-    video.classList.add('video-wrapper--enabled');
-  }
+    function findVideos() {
+      let videos = document.querySelectorAll('.video__wrapper');
+      
+      for (var i = 0; i < videos.length; i++) {
+        setupVideo(videos[i]);
+      }
+    }
 
-  function parseURL(data) {
-    let regexp = /https:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-    let url = data.src;
-    let match = url.match(regexp);
-    
-    return match[1];
-  }
+    function setupVideo(video) {
+      let link = video.querySelector('.video__link');
+      let image = video.querySelector('.video__image');
+      let button = video.querySelector('.video__button');
+      let id = parseURL(image);
+      
+      video.addEventListener('click', function() {
+        let iframe = createIframe(id);
+        
+        link.remove();
+        button.remove();
+        video.appendChild(iframe);
+      });
+      
+      link.removeAttribute('href');
+      video.classList.add('video-wrapper--enabled');
+    }
 
-  function createIframe(id) {
-    let iframe = document.createElement('iframe');
-    
-    iframe.setAttribute('allowfullscreen', '');
-    iframe.setAttribute('allow', 'autoplay');
-    iframe.setAttribute('src', generateURL(id));
-    iframe.classList.add('video__image');
-    
-    return iframe;
-  }
+    function parseURL(data) {
+      let regexp = /https:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+      let url = data.src;
+      let match = url.match(regexp);
+      
+      return match[1];
+    }
 
-  function generateURL(id) {
-    let query = '?rel=0&showinfo=0&autoplay=1';
-    
-    return 'https://www.youtube.com/embed/' + id + query;
-  }
+    function createIframe(id) {
+      let iframe = document.createElement('iframe');
+      
+      iframe.setAttribute('allowfullscreen', '');
+      iframe.setAttribute('allow', 'autoplay');
+      iframe.setAttribute('src', generateURL(id));
+      iframe.classList.add('video__image');
+      
+      return iframe;
+    }
+
+    function generateURL(id) {
+      let query = '?rel=0&showinfo=0&autoplay=1';
+      
+      return 'https://www.youtube.com/embed/' + id + query;
+    }
+  }, 300);
 })();
--- wc.js
+++ wc.js
@@ -5,63 +5,60 @@
     constructor() {
       super();
       
-      let shadow = this.attachShadow({mode: 'open'});
+      let shadow = this.attachShadow({
+        mode: 'open'
+      });
       
-      let wrapper = document.createElement('div');
-      let input = document.createElement('input');
-      let button = document.createElement('button');
-      // let script = document.createElement('script');
-      let style = document.createElement('style');
-      
-      wrapper.setAttribute('class', 'nav-bar__item');
-      input.setAttribute('type', 'text');
-      button.setAttribute('type', 'button');
-      button.textContent = 'Поиск';
-      // script.setAttribute('type', 'text/javascript');
-      
-      let placeholder = this.getAttribute('placeholder');
-      let classInput = this.getAttribute('classInput');
-      let classButton = this.getAttribute('classButton');
-      // let srcScript = this.getAttribute('srcScript');
-      input.placeholder = placeholder;
-      input.className = classInput;
-      button.className = classButton;
-      // script.src = srcScript;
-      
-      style.textContent = `
-        .nav-bar__item {
-          display: flex;
-          width: 300px;
-        }
-        
-        .nav-bar__input {
-          box-sizing: border-box;
-          width: 230px;
-          height: 30px;
-          box-shadow: 0 0 3px rgba(146,140,140,140.5);
-          border: none;
-          font-size: 14px;
+      let template = document.createElement('template');
+      template.innerHTML = `
+        <style>
+        input {
+          width: 270px;
+          height: 44px;
+          border: 1px solid #dfe1e5;
+          box-shadow: none;
+          font-size: 16px;
           color: #bfb5b5;
           margin-right: 10px;
-          padding-left: 5px;
+          padding-left: 20px;
+          padding-right: 20px;
+          border-radius: 24px;
+          outline: none;
+          box-sizing: border-box;
         }
         
-        .nav-bar__button {
-          box-sizing: border-box;
-          height: 30px;
-          padding: 7px;
-          border: none;
-          box-shadow: 0 0 3px rgba(146, 140, 140, 1);
-          color: #bfb5b5;
-          font-size: 14px;
+        input:hover, input:focus, button:hover, button:focus {
+          box-shadow: 0 1px 6px 0 rgba(32,33,36,0.28);
+          border-color: rgba(223,225,229,0);
         }
-      `
+
+        button {
+          padding: 7px;
+          border: 1px solid #dfe1e5;
+          box-shadow: none;
+          color: #bfb5b5;
+          font-size: 16px;
+          height: 44px;
+          border-radius: 24px;
+          outline: none;
+          box-sizing: border-box;
+        }
+        </style>
+        <div class="nav-bar__item">
+          <input type="text" name="search" value="" placeholder="">
+          <button type="button" id="" name="button">Поиск</button>
+        </div>
+      `;
       
-      shadow.appendChild(wrapper);
-      wrapper.appendChild(style);
-      wrapper.appendChild(input);
-      wrapper.appendChild(button);
-      // wrapper.appendChild(script);
+      shadow.appendChild(template.content.cloneNode(true));
+      
+      let button = shadow.querySelector('button');
+      let input = shadow.querySelector('input');
+      let type = this.getAttribute('type');
+      let placeHolder = this.getAttribute('placeholder');
+      input.placeholder = placeHolder;
+      button.id = type;
+      input.id = type;
     }
   }
   customElements.define('query-element', QueryElement);
