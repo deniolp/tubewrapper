bbe24b1520cb3b1fd470f8fe9c28697f17a8b539
Denis Popov <deniolp@gmail.com>
2018-12-14 07:45:04 +0300

    Beautify

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use stricts';
 
-(function () {
-  setTimeout (function() {
+(function() {
+  setTimeout(function() {
     let CHANNEL_QUERY = 'order=date&channelId=';
     let SEARCH_QUERY = 'q=';
-    
+
     let videoList = document.querySelector('.videos');
     let shadows = document.querySelectorAll('query-element');
     let buttons = [];
@@ -17,7 +17,7 @@
 
     function addVideoToList(video) {
       let template = document.querySelector('#template').content.querySelector('.video');
-      
+
       fragment.appendChild(createVideoElement(template, video));
     }
 
@@ -28,7 +28,7 @@
       element.querySelector('.video__picture').srcset = 'https://img.youtube.com/vi_webp/' + object.id.videoId + '/maxresdefault.webp';
       element.querySelector('.video__image').src = 'https://img.youtube.com/vi/' + object.id.videoId + '/maxresdefault.jpg';
       element.querySelector('.video__image').alt = object.snippet.title;
-      
+
       return element;
     }
 
@@ -36,7 +36,7 @@
       return new Promise(function(resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
-        
+
         xhr.addEventListener('load', function() {
           if (xhr.status === 200) {
             let json = xhr.response.items;
@@ -46,38 +46,39 @@
             reject(xhr.statusText);
           }
         });
-        
+
         xhr.addEventListener('error', function(error) {
           reject(error);
         });
-        
+
         xhr.open('GET', url);
         xhr.send();
       });
     }
-    
+
     function removeVideos() {
-      while(videoList.firstChild) {
+      while (videoList.firstChild) {
         videoList.removeChild(videoList.firstChild);
       }
     }
-    
+
     function fillVideoList(query, search) {
-      getData('https://www.googleapis.com/youtube/v3/search?part=snippet,id&' + query +  search + '&maxResults=50&key=AIzaSyC9j5myBqjEoydyrootsBO1iqe9-dSpPaA')
-        .then(function (videos) {
-          videos.forEach(function (video) {
+      getData('https://www.googleapis.com/youtube/v3/search?part=snippet,id&' + query + search + '&maxResults=50&key=AIzaSyC9j5myBqjEoydyrootsBO1iqe9-dSpPaA')
+        .then(function(videos) {
+          videos.forEach(function(video) {
             if (video.id.kind === 'youtube#video') {
               addVideoToList(video);
-            }});
+            }
+          });
           videoList.appendChild(fragment);
           findVideos();
         })
         .catch(error => console.log(error));
     }
-    
+
     function findVideos() {
       let videos = document.querySelectorAll('.video__wrapper');
-      
+
       for (var i = 0; i < videos.length; i++) {
         setupVideo(videos[i]);
       }
@@ -88,15 +89,15 @@
       let image = video.querySelector('.video__image');
       let button = video.querySelector('.video__button');
       let id = parseURL(image);
-      
+
       video.addEventListener('click', function() {
         let iframe = createIframe(id);
-        
+
         link.remove();
         button.remove();
         video.appendChild(iframe);
       });
-      
+
       link.removeAttribute('href');
       video.classList.add('video-wrapper--enabled');
     }
@@ -105,27 +106,27 @@
       let regexp = /https:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
       let url = data.src;
       let match = url.match(regexp);
-      
+
       return match[1];
     }
 
     function createIframe(id) {
       let iframe = document.createElement('iframe');
-      
+
       iframe.setAttribute('allowfullscreen', '');
       iframe.setAttribute('allow', 'autoplay');
       iframe.setAttribute('src', generateURL(id));
       iframe.classList.add('video__image');
-      
+
       return iframe;
     }
 
     function generateURL(id) {
       let query = '?rel=0&showinfo=0&autoplay=1';
-      
+
       return 'https://www.youtube.com/embed/' + id + query;
     }
-    
+
     buttons.forEach(function(button) {
       if (button.id === 'channel') {
         button.addEventListener('click', function() {
@@ -133,10 +134,10 @@
           if (search !== '') {
             removeVideos();
             fillVideoList(CHANNEL_QUERY, search);
-            }
+          }
         });
       }
-      
+
       if (button.id === 'search') {
         button.addEventListener('click', function() {
           let search = inputs[1].value;
@@ -144,7 +145,7 @@
             console.log(search);
             removeVideos();
             fillVideoList(SEARCH_QUERY, search);
-            }
+          }
         });
       }
     });
