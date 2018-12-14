f0a688ed754f00ab3228901719669d3728ef2060
Denis Popov <deniolp@gmail.com>
2018-12-14 09:22:05 +0300

    Создание кастомного элемента popup-info

--- wc.js
+++ wc.js
@@ -62,5 +62,73 @@
       input.id = type;
     }
   }
+  
+  class PopupInfo extends HTMLElement {
+    constructor() {
+      super();
+      
+      let shadow = this.attachShadow({
+        mode: 'open'
+      });
+      
+      let wrapper = document.createElement('span');
+      let icon = document.createElement('span');
+      let popup = document.createElement('span');
+      let text = this.getAttribute('text');
+      let img = document.createElement('img');
+      let style = document.createElement('style');
+      
+      wrapper.className = 'wrapper';
+      icon.className = 'icon';
+      icon.setAttribute('tabindex', 0);
+      popup.className = 'popup';
+      popup.textContent = text;
+      img.src = 'media/info.png';
+      icon.appendChild(img);
+      
+      style.textContent = `
+        .wrapper {
+          position: relative;
+        }
+        
+        .popup {
+          font-size: 5px;
+          width: 200px;
+          display: inline-block;
+          border: solid 1px black;
+          padding: 8px;
+          background: #ffffff;
+          border-radius: 10px;
+          opacity: 0;
+          transition: 0.6s all;
+          position: absolute;
+          bottom: 20px;
+          left: 10px;
+          z-index: 4; 
+        }
+        
+        .icon {
+          position: absolute;
+          top: 37px;
+          left: 327px;
+        }
+        
+        img {
+          width: 20px;
+        }
+        
+        .icon:hover + .popup, icon:focus + .popup {
+          opacity: 1;
+        }
+      `
+      
+      shadow.appendChild(style);
+      shadow.appendChild(wrapper);
+      shadow.appendChild(icon);
+      shadow.appendChild(popup);
+    }
+  }
+  
   customElements.define('query-element', QueryElement);
+  customElements.define('popup-info', PopupInfo);
 })();
