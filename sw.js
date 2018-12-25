b9add9e6e89952b92732a0846d60de1873a2fa27
Denis Popov <deniolp@gmail.com>
2018-12-25 19:40:44 +0300

    Проверка

--- sw.js
+++ sw.js
@@ -6,24 +6,6 @@ let urlsToCache = [
   'media/broken.png'
 ];
 
-<<<<<<< HEAD
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-      caches.open(CACHE_NAME)
-      .then((cache) => {
-          return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Activated!');
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch event!');
-});
-=======
   self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -41,4 +23,3 @@ self.addEventListener('fetch', (event) => {
     console.log('Fetch event!');
   });
 })();
->>>>>>> 8f1260e53fbc80d6bafab7ee195dcd8b8bfbf8c6
