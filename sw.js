ce33fde55df28969ca97ba8b78dc4985fdc0ca99
Denis Popov <deniolp@gmail.com>
2018-12-25 12:37:41 +0300

    Реализация ливрелоада для более удобной работы

--- sw.js
+++ sw.js
@@ -1,42 +1,44 @@
 'use strict';
 
-let CACHE_NAME = 'my-site-cache';
-let urlsToCache = [
-  'media/broken.png'
-];
+(function() {
+  let CACHE_NAME = 'my-site-cache';
+  let urlsToCache = [
+    'media/broken.png'
+  ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-      caches.open(CACHE_NAME)
-      .then((cache) => {
-          return cache.addAll(urlsToCache);
-      })
-  );
-});
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+  });
 
-self.addEventListener('activate', (event) => {
-  console.log('Activated!');
-});
+  self.addEventListener('activate', (event) => {
+    console.log('Activated!');
+  });
 
-function isImage(fetchRequest) {
-    return fetchRequest.method === 'GET'
-           && fetchRequest.destination === 'image';
-}
+  function isImage(fetchRequest) {
+      return fetchRequest.method === 'GET'
+             && fetchRequest.destination === 'image';
+  }
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then(function(response) {
-        if (response.status === 200) {
-          return response;
-        } else {
-          if (isImage(event.request)) {
-            return caches.match('media/broken.png');
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      fetch(event.request)
+        .then(function(response) {
+          if (response.status === 200) {
+            return response;
+          } else {
+            if (isImage(event.request)) {
+              return caches.match('media/broken.png');
+            }
           }
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  )
-});
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    )
+  });
+})();
